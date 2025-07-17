1f8f980ede0e58c1f23fdbd6abf5a5b738d3b36a
christian <christianbach1689@googlemail.com>
2025-07-17 14:27:21 +0200

    Update scripts

--- scroll-phat/scroll-text-garden-state.py
+++ scroll-phat/scroll-text-garden-state.py
@@ -1,50 +1,47 @@
-#!/usr/bin/env python
-import paho.mqtt.client as mqtt
 import scrollphat
-import sys
-import time
+import requests
 import json
+import time
+from datetime import datetime
+import sys
 
-poolSensor = "WaterSensor_Pool"
-client = mqtt.Client()
+# Rotate 180 degrees and set brightness
+scrollphat.set_rotate(True)
+scrollphat.set_brightness(3)
 
-def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
-    # Subscribe, which need to put into on_connect
-    # If reconnect after losing the connection with the broker, it will continue to subscribe to the raspberry/topic topic
-    client.subscribe("zigbee2mqtt/" + poolSensor)
+hostname="http://192.168.188.21:3000"
 
-    print(
-    """
-    Scroll pHAT - Scrolling Text
+def scroll():
+  temperaturePool = request(hostname+"/pool","temperature")
+  responseSoilMoistureVegetables = request(hostname+"/soilMoistureVegetables")
+  responseSoilMoistureGreenhouse = request(hostname+"/soilMoistureGreenhouse")
+  responseTmpGreenhouse = request(hostname+"/temperature-greenhouse")
+  actualTime = datetime.now().strftime("%H:%M")
+  message = actualTime+"Uhr Pool "+temperaturePool+"C Boden "+str(responseSoilMoistureVegetables["temperature"])+"C "+str(responseSoilMoistureVegetables["soil_moisture"])+"% Bodenfeuchte Gewaechshaus "+str(responseTmpGreenhouse["temperature"])+"C "+str(responseTmpGreenhouse["humidity"])+"% Luftfeuchte "+str(responseSoilMoistureGreenhouse["soil_moisture"])+"% Bodenfeuchte";
+  print(message)
+  time.sleep(5)
+ # message="22:10 Uhr Pool 37C Boden 25C 50% Bodenfeuchte Gewaechshaus 25C 60% Luftfeuchte 80% Bodenfeuchte"
+#  scrollphat.write_string(message.upper(), 11)
+  length = scrollphat.buffer_len()
 
-    Press Ctrl+C to exit!
+  for i in range(length):
+    try:
+        scrollphat.scroll()
+        time.sleep(0.1)
+    except KeyboardInterrupt:
+        scrollphat.clear()
+        sys.exit(-1)
 
-    """
-    )
 
-    scrollphat.set_brightness(1)
+def request(endpoint,propertyName=""):
+  response = requests.get(endpoint)
+  jsonResponse = json.loads(str(response.content.decode("utf-8")))
 
-    while True:
-        try:
-            scrollphat.scroll()
-            time.sleep(0.1)
-        except KeyboardInterrupt:
-            scrollphat.clear()
-            sys.exit(-1)
+  if(propertyName != ""):
+    return str(jsonResponse[propertyName])
+  return jsonResponse
 
-def on_message(client, userdata, msg):
-    decoded_message = str(msg.payload.decode("utf-8"))
-    msgjson = json.loads(decoded_message)
-    temperature = msgjson["temperature"]
-    print("temperature: " + str(temperature))
-    scrollingText = "Pool " + str(int(temperature)) + "GRAD"
-    scrollphat.write_string(scrollingText.upper(), 11)
+while True:
+  scroll()
 
-client.on_connect = on_connect
-client.on_message = on_message
-
-# Create connection, the three parameters are broker address, broker port number, and keep-alive time respectively
-client.connect("192.168.188.21", 1883, 60)
-
-client.loop_forever()
+scroll()
