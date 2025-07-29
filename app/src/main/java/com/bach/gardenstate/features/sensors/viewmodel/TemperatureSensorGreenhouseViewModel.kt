f0dbb8537158c7594b7930cdaa288d10350a1259
christian <christianbach1689@googlemail.com>
2025-07-29 22:41:34 +0200

    Fix bug

--- app/src/main/java/com/bach/gardenstate/features/sensors/viewmodel/TemperatureSensorGreenhouseViewModel.kt
+++ app/src/main/java/com/bach/gardenstate/features/sensors/viewmodel/TemperatureSensorGreenhouseViewModel.kt
@@ -6,11 +6,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bach.gardenstate.features.sensors.data.ApiClient
+import com.bach.gardenstate.features.sensors.model.TemperatureSensorData
 import com.bach.gardenstate.features.sensors.model.TemperatureSensorUIState
+import com.bach.gardenstate.features.sensors.model.WaterSensorPoolData
 import io.ktor.client.call.body
 import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
 
 class TemperatureSensorGreenhouseViewModel : ViewModel() {
+    private val withUnknownKeys = Json { ignoreUnknownKeys = true }
     private val _messageTemperatureSensor: MutableState<TemperatureSensorUIState> = mutableStateOf(
         TemperatureSensorUIState.isLoading
     )
@@ -21,7 +25,11 @@ class TemperatureSensorGreenhouseViewModel : ViewModel() {
             val httpResponse = ApiClient.getActualTemperature()
             when (httpResponse.status.value) {
                 200 -> _messageTemperatureSensor.value =
-                    TemperatureSensorUIState.success(httpResponse.body())
+                    TemperatureSensorUIState.success(
+                        withUnknownKeys.decodeFromString<TemperatureSensorData>(
+                            httpResponse.body()
+                        )
+                    )
             }
         }
     }
